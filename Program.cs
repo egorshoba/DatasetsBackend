d3fc5ef0c13cb960e1741a63790fa61cd282936d
Egor Shoba <egorshoba@outlook.com>
2022-01-04 18:49:31 +0500

    Configuration has been added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace DatasetsBackend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = BuildWebApplication(args);
 
-app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello World!");
 
-app.Run();
+            app.Run();
+        }
+
+        public static WebApplication BuildWebApplication(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+
+            IHostEnvironment env = builder.Environment;
+            var jsonFileName = $"appsettings.{env.EnvironmentName}.json";
+            builder.Configuration.AddJsonFile(jsonFileName, optional: true, reloadOnChange: true);
+
+            var connectionString = builder.Configuration.GetConnectionString("DatasetsBackendDbContext");
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            return builder.Build();
+        }
+    }
+}
