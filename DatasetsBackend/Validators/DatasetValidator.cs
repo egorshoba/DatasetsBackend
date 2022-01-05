4547b6b7986c3d29eb47af786bc89e8dd247cd90
Egor Shoba <egorshoba@outlook.com>
2022-01-05 13:47:22 +0500

    Dataset name validator has been added
    Tests added also

---
+++ DatasetsBackend/Validators/DatasetValidator.cs
@@ -0,0 +1,34 @@
+﻿using DatasetsBackend.Dtos;
+using System.Text.RegularExpressions;
+
+namespace DatasetsBackend.Validators
+{
+    public class DatasetValidator
+    {
+        readonly UploadDatasetDto DatasetMetadata;
+        readonly IFormFile File;
+        List<string> Errors;
+
+        public DatasetValidator(UploadDatasetDto datasetMetadata, IFormFile file)
+        {
+            DatasetMetadata = datasetMetadata;
+            File = file;
+            Errors = new();
+        }
+
+        public IEnumerable<string> GetValidationErrors()
+        {
+            NameIsLatin();
+            return Errors;
+        }
+
+        void NameIsLatin()
+        {
+            var regex = new Regex(@"^[A-z]+$", RegexOptions.IgnoreCase);
+            if (!regex.IsMatch(DatasetMetadata.Name))
+            {
+                Errors.Add("Name should contain only latin chars");
+            }
+        }
+    }
+}
---
+++ DatasetsBackend_Tests/DatasetValidator_Tests.cs
@@ -0,0 +1,69 @@
+﻿using DatasetsBackend.Dtos;
+using DatasetsBackend.Validators;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DatasetsBackend_Tests
+{
+    public class DatasetValidator_Tests
+    {
+        IFormFile File;
+
+        public DatasetValidator_Tests()
+        {
+            var bytes = Encoding.UTF8.GetBytes("This is a dummy file");
+            File = new FormFile(new MemoryStream(bytes), 0, bytes.Length, "Data", "dummy.txt");
+        }
+
+        [Fact]
+        public void NameIsLatin()
+        {
+            var dto = new UploadDatasetDto
+            {
+                Name = "Test"
+            };
+
+            var validator = new DatasetValidator(dto, File);
+
+            var validationErrors = validator.GetValidationErrors();
+
+            Assert.Empty(validationErrors);
+        }
+
+        [Fact]
+        public void NameIsNotLatin()
+        {
+            var dto = new UploadDatasetDto
+            {
+                Name = "teыt"
+            };
+
+            var validator = new DatasetValidator(dto, File);
+
+            var validationErrors = validator.GetValidationErrors();
+
+            Assert.Collection(validationErrors, item => Assert.Contains("Name should contain only latin chars", item));
+        }
+
+        [Fact]
+        public void NameContainsDigit()
+        {
+            var dto = new UploadDatasetDto
+            {
+                Name = "Test1"
+            };
+
+            var validator = new DatasetValidator(dto, File);
+
+            var validationErrors = validator.GetValidationErrors();
+
+            Assert.Collection(validationErrors, item => Assert.Contains("Name should contain only latin chars", item));
+        }
+    }
+}
